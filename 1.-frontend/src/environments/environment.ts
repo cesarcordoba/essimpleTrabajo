53a51b75b13e7d5dbb37a3d806f463cc9f2a860c
Your Name <you@example.com>
2019-02-14 14:38:52 -0600

    otra vez subido 14 de febrero 2019 v2

--- 1.-frontend/src/environments/environment.ts
+++ 1.-frontend/src/environments/environment.ts
@@ -20,7 +20,7 @@ export const APILOCAL = {
 
 
 export const AWSKEYS = {
-  accessKeyId: 'AKIAJEYX66PCBH6V4VHQ',
-  secretAccessKey: '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',
+  accessKeyId: 'AKIAJEYX66//////////////PCBH6V4VHQ',
+  secretAccessKey: 'Vne0oFxY2Dorq0Wl9////////////////vNdDLA3J05yENUfqlQr0UfW',
   region: 'us-east-1'
 }
